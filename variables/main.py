ff87bf40e6abf1ade8d05dc13c12043b1e29b3bc
ZencakeTheBob <bkloes@kodision.nl>
2022-11-11 15:39:38 +0100

    dit zijn de vars

---
+++ variables/main.py
@@ -0,0 +1,8 @@
+# Do not modify these lines
+__winc_id__ = '7152c06aa3ac4d5f964ca8619ecb7e8f'
+__human_name__ = 'variables'
+
+# Add your code after this line
+number = 3
+name = "Aapman"
+bool = True
