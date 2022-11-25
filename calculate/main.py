dfc3bbd736af9767dd00b4f43b160d44a0426c58
ZencakeTheBob <bkloes@kodision.nl>
2022-11-25 16:23:34 +0100

    Te moeilijk voor een te simpele opdracht :)

--- calculate/main.py
+++ calculate/main.py
@@ -3,16 +3,43 @@ __winc_id__ = '499e67d5cb54448e93cee7465be2c866'
 __human_name__ = 'calculate'
 
 # Add your code after this line
-veg1 = 'broccoli', 2
-veg2 = 'leek', 2
-veg3 = 'potato', 3
-veg4 = 'brussel_sprout', 7
+broccoli = 2
+leek = 2
+potato = 3
+brussel_sprout = 7
+vegetables = [broccoli, leek, potato, brussel_sprout]
+price_vegetables = [2, 2, 3, 7]
+num_potatoes = 7
+num_broccolis = 5
+num_leeks = 2
+num_brussel_sprouts = 10
+desired_num_veg = [num_potatoes * 3, num_leeks * 2, num_broccolis * 2, num_brussel_sprouts * 7]
+discount_percentage = 30
 
-prices =  2, 3, 7
-vegies = ["broccoli", 2], ["leek", 2], ["potato", 3], ["brussel_sprout", 7]
 
-sum_one_each = 0
-for sum_one_each in vegies:
-    sum_one_each += veg
+def sum_of_one_each():
+    Sum_one_each = sum(vegetables)
+    return Sum_one_each
 
-sum_one_each
+def average_price():
+    total_price = sum(price_vegetables)
+    count = len(price_vegetables)
+    avg_price = total_price / count
+    return avg_price
+
+def sum_of_total():
+    desired_total = sum(desired_num_veg)
+    return desired_total
+
+def discounted_of_sum_total():
+    discounted_price = discount_percentage / 100 * sum_of_total()
+    total_price = sum_of_total() - discounted_price
+    total_price = round(total_price, 2)
+    return total_price
+
+sum_one_each = sum_of_one_each()
+avg_price = average_price()
+sum_total = sum_of_total()
+discounted_sum_total = discounted_of_sum_total()
+
+print(discounted_sum_total)
