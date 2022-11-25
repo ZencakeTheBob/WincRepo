c52c57d054bc0cef7712d326133a2af800ae0b7d
ZencakeTheBob <bkloes@kodision.nl>
2022-11-25 11:41:35 +0100

    dit zijn de ints, bool en strings

---
+++ calculate/main.py
@@ -0,0 +1,18 @@
+# Do not modify these lines
+__winc_id__ = '499e67d5cb54448e93cee7465be2c866'
+__human_name__ = 'calculate'
+
+# Add your code after this line
+veg1 = 'broccoli', 2
+veg2 = 'leek', 2
+veg3 = 'potato', 3
+veg4 = 'brussel_sprout', 7
+
+prices =  2, 3, 7
+vegies = ["broccoli", 2], ["leek", 2], ["potato", 3], ["brussel_sprout", 7]
+
+sum_one_each = 0
+for sum_one_each in vegies:
+    sum_one_each += veg
+
+sum_one_each
